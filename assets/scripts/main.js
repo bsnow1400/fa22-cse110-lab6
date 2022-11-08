146d3067dac5b16fa4c659db7a5c713a36c9b9e4
bsnow1400 <63069372+bsnow1400@users.noreply.github.com>
2022-11-07 19:45:25 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -69,8 +69,7 @@ function saveRecipesToStorage(recipes) {
 function initFormHandler() {
 
   // B2. TODO - Get a reference to the <form> element
-  const form = document.getElementById("new-recipe");
-  const main = document.querySelector("main");
+  let form = document.querySelector('form');
   
   // B3. TODO - Add an event listener for the 'submit' event, which fires when the
   //            submit button is clicked
